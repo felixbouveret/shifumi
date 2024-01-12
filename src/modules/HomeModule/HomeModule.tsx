65768a7a59fbde9281547ce5d3d2d2fc9ee3db53
Félix Bouveret <felix@getluko.com>
2024-01-12 15:14:48 +0100

    Create first home

--- src/modules/HomeModule/HomeModule.tsx
+++ src/modules/HomeModule/HomeModule.tsx
@@ -1,10 +1,25 @@
 import "./HomeModule.scss";
 import React from "react";
+import { Button, IconButton } from "@mui/joy";
+import { People, Settings } from "@mui/icons-material";
 
 const HomeModule: React.FC = () => {
   return (
-    <div>
+    <div className="container">
       <h1>Shifumi</h1>
+      <Button className="button" size="lg">
+        Play
+      </Button>
+
+      <div className="iconButtonsContainer">
+        <IconButton className="IconButton" size="lg">
+          <People />
+        </IconButton>
+
+        <IconButton className="IconButton" size="lg">
+          <Settings />
+        </IconButton>
+      </div>
     </div>
   );
 };
