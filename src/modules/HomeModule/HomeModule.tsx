97ce75425f99f915e98a33e56dacb2e7b9bc6c38
Félix Bouveret <felix@getluko.com>
2024-01-12 15:04:57 +0100

    Initialize Shifumi app

---
+++ src/modules/HomeModule/HomeModule.tsx
@@ -0,0 +1,12 @@
+import "./HomeModule.scss";
+import React from "react";
+
+const HomeModule: React.FC = () => {
+  return (
+    <div>
+      <h1>Shifumi</h1>
+    </div>
+  );
+};
+
+export default HomeModule;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import HomeModule from "../modules/HomeModule/HomeModule";
+
+const Home: React.FC = () => <HomeModule />;
+
+export default Home;
