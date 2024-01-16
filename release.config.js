0ba7cbb36de792b6efb19a3e10a2705735e1f76b
Félix Bouveret <felix@getluko.com>
2024-01-16 10:48:35 +0100

    Add semantic release config

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/npm",
+      {
+        assets: ["package.json"],
+        npmPublish: false,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
